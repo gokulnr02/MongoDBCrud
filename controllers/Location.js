0df3f53d82a7245a2d59816fe194313a33208c7e
GOKUL NR <118470070+gokulnr02@users.noreply.github.com>
2024-10-03 00:41:56 +0530

    locationCountry

--- controllers/Location.js
+++ controllers/Location.js
@@ -1,5 +1,8 @@
-const Location = require('../MongoseDB/LocationSchema')
+const Location = require('../MongoseDB/LocationSchema');
+const CitySchema = require('../MongoseDB/CitySchema');
+const CountrySchema = require('../MongoseDB/CountrySchema');
 
+const mongoose = require('mongoose')
 exports.LocationSave = async (request, response) => {
     try {
 
@@ -10,38 +13,47 @@ exports.LocationSave = async (request, response) => {
 
 exports.LocationSelect = async (request, response) => {
     try {
-        const result = await Location.aggregate([
+        //  const city =await CitySchema.where("_id").equals("66fadc1469f97a4c3a6e5803")
+        const CountryMID = "66fadb2669f97a4c3a6e57f5";
+        const pipeline = [];
+        const match = {};
+
+        if(CountryMID){
+            match.CountryMID =new mongoose.Types.ObjectId(CountryMID)
+        }
+
+        pipeline.push(
             {
-                $match: {}
+                $match: match
             },
             {
                 $lookup: {
-                    from: 'cities',
-                    localField: '_id',
+                    from: 'countrysaves',
+                    localField: 'CountryMID',
                     foreignField: '_id',
-                    as: 'CityDocuments'
+                    as: 'country'
                 }
             },
             {
-                $project: {
-                    CityDocuments: 1
+                $unwind: { path: "$country", preserveNullAndEmptyArrays: true }
+            },
+            {
+                $project: { __v: 0 }
+            },
+            {
+                $addFields: {
+                    "CountryName": "$country.CountryName",
+                    "CountryCode": "$country.CountryCode",
                 }
             },
             {
-                $unwind: {
-                    path: '$CityDocuments',
-                    preserveNullAndEmptyArrays: false
-                }
-            },
-            {
-                $group: {
-                    '_id': '_id',
-                    cities: { $push: "$CityDocuments" }
-                }
+                $project: { country: 0 }
             }
+        );
 
-        ])
-        return response.send(result)
+        const city = await CitySchema.aggregate(pipeline);
+
+        return response.send(city)
     } catch (err) {
         console.log(err, "Error")
     }
