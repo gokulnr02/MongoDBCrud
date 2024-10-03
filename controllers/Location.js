f28e2ae4f20f4d57f838a96e92fb4771f626eae8
GOKUL NR <118470070+gokulnr02@users.noreply.github.com>
2024-10-03 23:53:25 +0530

    cityAndcountry

--- controllers/Location.js
+++ controllers/Location.js
@@ -14,47 +14,76 @@ exports.LocationSave = async (request, response) => {
 exports.LocationSelect = async (request, response) => {
     try {
         //  const city =await CitySchema.where("_id").equals("66fadc1469f97a4c3a6e5803")
-        const CountryMID = "66fadb2669f97a4c3a6e57f5";
-        const pipeline = [];
-        const match = {};
-
-        if(CountryMID){
-            match.CountryMID =new mongoose.Types.ObjectId(CountryMID)
+        const json = request.body.data
+        switch (json.type) {
+            case 'CitySelect':
+                await citySelect(json, response);
+                break;
+            case 'CountrySelect':
+                await CountrySelect(json, response);
+                break;
+            default:
+                // await TableSelect(json, response)
+                break;
         }
-
-        pipeline.push(
-            {
-                $match: match
-            },
-            {
-                $lookup: {
-                    from: 'countrysaves',
-                    localField: 'CountryMID',
-                    foreignField: '_id',
-                    as: 'country'
-                }
-            },
-            {
-                $unwind: { path: "$country", preserveNullAndEmptyArrays: true }
-            },
-            {
-                $project: { __v: 0 }
-            },
-            {
-                $addFields: {
-                    "CountryName": "$country.CountryName",
-                    "CountryCode": "$country.CountryCode",
-                }
-            },
-            {
-                $project: { country: 0 }
-            }
-        );
-
-        const city = await CitySchema.aggregate(pipeline);
-
-        return response.send(city)
     } catch (err) {
         console.log(err, "Error")
     }
+}
+
+const citySelect = async(json,response)=>{
+    const pipeline = [];
+    const match = {};
+    if(json.CountryMID){
+        match.CountryMID =new mongoose.Types.ObjectId(json.CountryMID)
+    }
+    pipeline.push(
+        {
+            $match: match
+        },
+        {
+            $lookup: {
+                from: 'countrysaves',
+                localField: 'CountryMID',
+                foreignField: '_id',
+                as: 'country'
+            }
+        },
+        {
+            $unwind: { path: "$country", preserveNullAndEmptyArrays: true }
+        },
+        {
+            $addFields: {
+                "CountryName": "$country.CountryName",
+                "CountryCode": "$country.CountryCode",
+            }
+        },
+        {
+            $project: { country: 0 , __v: 0}
+        }
+    );
+
+    const city = await CitySchema.aggregate(pipeline);
+    const Totalcounts = city.length
+    const result= city.map(val =>({
+       ...val,Totalcounts
+     }))
+    return response.send(result)
+}
+
+const CountrySelect = async(json,response)=>{
+    const {index,page} = json
+    let country = await CountrySchema.find({},{__v:0})
+                  .skip((index-1)*page)
+                  .limit(page)
+    country = country.map(({ _id, CountryCode, CountryName, CountryID, currentDate, UpdatedtDate },i,array) => ({
+        _id,
+        CountryCode,
+        CountryName,
+        CountryID,
+        currentDate,
+        UpdatedtDate,
+        Totalcounts:array.length,
+      }));
+    return response.send(country)
 }
